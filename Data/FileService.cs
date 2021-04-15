f7a07780a1f8a4189071f0ad574e1bee9adae534
Geerten Vink <maedaros@yahoo.com>
2021-04-15 22:12:21 +0200

    Add project files.

---
+++ Data/FileService.cs
@@ -0,0 +1,22 @@
+﻿using LijstenMam.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LijstenMam.Data
+{
+    public class FileService
+    {
+        public async Task<List<File>> GetFiles()
+        {
+            var files = new List<File>()
+            {
+                new File() {Name = "Boeken" },
+                new File() {Name = "Tijdschriften" }
+            };
+
+            return await Task.FromResult(files);
+        }
+    }
+}
---
+++ Shared/File.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LijstenMam.Shared
+{
+    public class File
+    {
+        public string Name { get; set; }
+    }
+}

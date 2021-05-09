f236e28f64ca439746f892923e522517560b117e
Geerten Vink <maedaros@yahoo.com>
2021-05-09 14:22:22 +0200

    only search at the level the data is relevant

--- Backend/ElasticSearch/DocumentConverter.cs
+++ Backend/ElasticSearch/DocumentConverter.cs
@@ -29,20 +29,83 @@ namespace LijstenMam.Backend.ElasticSearch
 
         private void Convert(FileElement element, List<FileElementDTO> dtos)
         {
-            dtos.Add(new FileElementDTO()
-            {
-                ParagraphNumber = element.ParagraphNumber,
-                Genres = element.ElementData.Genres,
-                Compilers = element.ElementData.Compilers,
-                BookTitle = element.ElementData.BookTitle,
-                Authors = element.ElementData.Authors,
-                ArticleTitle = element.ElementData.ArticleTitle
-            });
+            FileElementDTO dto;
+            var canConvert = TryConvertGenre(element, out dto) ||
+                TryConvertBook(element, out dto) ||
+                TryConvertArticle(element, out dto) ||
+                TryConvertElement(element, out dto);
+
+            if (canConvert) dtos.Add(dto);
 
             foreach(var child in element.Children)
             {
                 Convert(child, dtos);
             }
+        }
+
+        private bool TryConvertGenre(FileElement element, out FileElementDTO dto)
+        {
+            dto = null;
+
+            if (element is Genre)
+            {
+                dto = new FileElementDTO()
+                {
+                    ParagraphNumber = element.ParagraphNumber,
+                    Genres = element.ElementData.Genres
+                };
+
+                return true;
+            }
+            return false;
+        }
+
+        private bool TryConvertBook(FileElement element, out FileElementDTO dto)
+        {
+            dto = null;
+
+            if (element is Book)
+            {
+                dto = new FileElementDTO()
+                {
+                    ParagraphNumber = element.ParagraphNumber,
+                    BookTitle = element.ElementData.BookTitle,
+                    Compilers = element.ElementData.Compilers
+                };
+                return true;
+            }
+            return false;
+        }
+
+        private bool TryConvertArticle(FileElement element, out FileElementDTO dto)
+        {
+            dto = null;
+
+            if (element is Article)
+            {
+                dto = new FileElementDTO()
+                {
+                    ParagraphNumber = element.ParagraphNumber,
+                    ArticleTitle = element.ElementData.ArticleTitle,
+                    Authors = element.ElementData.Authors
+                };
+                return true;
+            }
+            return false;
+        }
+
+        private bool TryConvertElement(FileElement element, out FileElementDTO dto)
+        {
+            dto = new FileElementDTO()
+            {
+                ParagraphNumber = element.ParagraphNumber,
+                Genres = element.ElementData.Genres,
+                BookTitle = element.ElementData.BookTitle,
+                Compilers = element.ElementData.Compilers,
+                ArticleTitle = element.ElementData.ArticleTitle,
+                Authors = element.ElementData.Authors
+            };
+            return true;
         }
     }
 }
